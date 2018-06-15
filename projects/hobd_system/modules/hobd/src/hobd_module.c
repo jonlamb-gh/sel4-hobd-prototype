d33c830852132914180bd705ecbe5e529933a258
jonlamb-gh <jonlambusn@gmail.com>
2018-06-15 10:18:11 -0700

    Example GPIO initialization

--- projects/hobd_system/modules/hobd/src/hobd_module.c
+++ projects/hobd_system/modules/hobd/src/hobd_module.c
@@ -10,13 +10,15 @@
 
 #include <sel4/sel4.h>
 #include <sel4debug/debug.h>
+#include <platsupport/io.h>
+#include <platsupport/mux.h>
+#include <platsupport/gpio.h>
 
 #include "init_env.h"
 #include "thread.h"
 #include "hobd_module.h"
 
 /* TODO - move to a config.h ? */
-
 #define THREAD_NAME "hobd-module"
 
 /* arbitrary (but unique) number for a badge */
@@ -25,8 +27,9 @@
 /* size of the thread's stack in words */
 #define THREAD_STACK_SIZE (512)
 
-static uint64_t g_thread_stack[THREAD_STACK_SIZE];
+static gpio_sys_t g_gpio_sys;
 static thread_s g_thread;
+static uint64_t g_thread_stack[THREAD_STACK_SIZE];
 
 static void thread_fn(void)
 {
@@ -44,7 +47,32 @@ static void thread_fn(void)
 void hobd_module_init(
         init_env_s * const env)
 {
+    int err;
+
     (void) memset(&g_thread, 0, sizeof(g_thread));
+    (void) memset(&g_gpio_sys, 0, sizeof(g_gpio_sys));
+
+    /* initialize the MUX subsytem */
+    err = mux_sys_init(
+            &env->io_ops,
+            NULL,
+            &env->io_ops.mux_sys);
+    ZF_LOGF_IF(err != 0, "Failed to initialize MUX subsystem\n");
+
+    /* initialize the GPIO subsytem */
+    err = gpio_sys_init(
+            &env->io_ops,
+            &g_gpio_sys);
+    ZF_LOGF_IF(err != 0, "Failed to initialize GPIO subsystem\n");
+
+    /* TODO - test out a port/pin combo */
+    gpio_t gpio;
+    err = gpio_new(
+            &g_gpio_sys,
+            GPIOID(GPIO_BANK1, 1),
+            GPIO_DIR_OUT,
+            &gpio);
+    ZF_LOGF_IF(err != 0, "Failed to initialize GPIO port/pin\n");
 
     /* create a new thread */
     thread_create(
